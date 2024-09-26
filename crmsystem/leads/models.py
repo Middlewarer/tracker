3f6bfa2653a74c3e5c63cf83754a2cbae50cc3c4
Grabchuk Alex <mranarchy@mail.ru>
2024-09-26 17:06:22 +0300

    New models created

--- crmsystem/leads/models.py
+++ crmsystem/leads/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=25)
+    
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=25)
     age = models.IntegerField()
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE, null=True)
+
+
