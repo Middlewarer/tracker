1583735e2e6c5006dfd75a876d2696f4cd29e346
Grabchuk Alex <mranarchy@mail.ru>
2024-09-26 18:34:09 +0300

    All till admin pannel was involved

--- crmsystem/leads/models.py
+++ crmsystem/leads/models.py
@@ -7,9 +7,10 @@ class User(AbstractUser):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=25)
-    
+
+    def __str__(self) -> str:
+        return self.user.username
+
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
@@ -17,4 +18,6 @@ class Lead(models.Model):
     age = models.IntegerField()
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE, null=True)
 
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
 
