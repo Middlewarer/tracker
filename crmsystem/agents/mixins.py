769d99dbaab8974b3af94164ba26b9d60adfb723
Grabchuk Alex <mranarchy@mail.ru>
2024-10-10 04:38:44 +0300

    Pre-last commit

---
+++ crmsystem/agents/mixins.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
+from django.shortcuts import redirect
+
+class OrganisorAndLoginRequiredMixin(AccessMixin):
+    """Verify that the current user is authenticated and is an organisor."""
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated or request.user.is_organisor:
+            return redirect('leads:lead_list')
+        return super().dispatch(request, *args, **kwargs)
+
--- crmsystem/agents/urls.py
+++ crmsystem/agents/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import AgentListView
+from .views import AgentUpdateView, AgentListView, AgentCreateView, AgentDetailView, AgentDeleteView
 
 app_name = 'agents'
 
 urlpatterns = [
-    path('', AgentListView.as_view(), name='agents')
+
+    path('', AgentListView.as_view(), name='agent_list'),
+    path('create/', AgentCreateView.as_view(), name='agent_create'),
+    path('<int:pk>/', AgentDetailView.as_view(), name='agent_detail'),
+    path('<int:pk>/update', AgentUpdateView.as_view(), name='agent_update'),
+    path('<int:pk>/delete', AgentDeleteView.as_view(), name='agent_delete'),
+    
 ]
